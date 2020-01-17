31307c7cb3e4a020ba316abadfc3cb21c19a5d84
Rahmat Eka <rahmat_eka@me.com>
2020-01-17 20:33:03 +0700

    init layout

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="app">
+    <a-layout id="components-layout-demo-top" class="layout" theme="light">
+        <a-layout-header>
+          <div class="logo">
+            <img src="./assets/CC-logo.svg" alt="Compare Club">
+          </div>
+          <div class="top-menus">
+            <a-icon type="user" />
+            <a-icon type="bars" />
+          </div>
+        </a-layout-header>
+        <a-layout-content style="padding: 0 50px">
+          <p>Dashboard</p>
+          <a-tabs defaultActiveKey="2">
+            <a-tab-pane key="1">
+              <span slot="tab">
+                <a-icon type="stock" />
+                Credit Score
+              </span>
+              Tab 1
+            </a-tab-pane>
+            <a-tab-pane key="2">
+              <span slot="tab">
+                <a-icon type="fire" />
+                Fuel
+              </span>
+              Tab 2
+            </a-tab-pane>
+            <a-tab-pane key="3">
+              <span slot="tab">
+                <a-icon type="shopping-cart" />
+                Groceries
+              </span>
+              <!-- <Hello></Hello> -->
+            </a-tab-pane>
+          </a-tabs>
+          <!-- <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+            <main>
+              <router-view></router-view>
+            </main>
+          </div> -->
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          Vue using Ant Design
+        </a-layout-footer>
+      </a-layout>
+</div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+
+.ant-layout-header {
+  background: #ffffff;
+  border-width: 0px 0px 3px 0px;
+  border-color: #70C298;
+  border-style: solid;
+}
+
+#components-layout-demo-top .logo {
+    width: 120px;
+    float: left;
+  }
+
+#components-layout-demo-top .top-menus {
+    float: right;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue'
+
+Vue.config.productionTip = false
+Vue.use(Antd)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
