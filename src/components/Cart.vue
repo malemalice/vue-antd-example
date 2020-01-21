bd1557eeb589c86ce9e352741a3c23ad93a77420
Rahmat Eka <rahmat_eka@me.com>
2020-01-21 21:57:25 +0700

    fix desktop layout

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="cart">
-    <a-row>
+    <a-row class="top-header" style="margin-bottom:10px">
       <a-col :span="12" class="label">My Shopping List</a-col>
     </a-row>
     <a-row>
-      <a-col :span="6" :offset="9">
+      <a-col :span="8" :offset="8">
         <div class="cart-item label" v-for="item in shoppingList">
           <a-row type="flex" justify="space-around" align="middle">
             <a-col :span="4">
               <img :src=item.imgUrl>
             </a-col>
-            <a-col :span="12">
-              <p>IMAGE BRAND</p>
+            <a-col :span="18">
+              <p><img src="../assets/coles_save.png"></p>
               <p>{{item.name}}</p>
               <a-row>
                 <a-col :span="12">
                   <span class="discount">Discount</span></br>{{item.discount ? item.discount: '0'}}%
                 </a-col>
-                <a-col :span="12" class="price" style="text-align:center">${{item.latestPrice}}</a-col>
+                <a-col :span="12" class="price" style="text-align:center">
+                  <span class="class-heigh-100">${{item.latestPrice}}</span>
+                </a-col>
               </a-row>
             </a-col>
-            <a-col :span="2" style="text-align:center;" >
+            <a-col :span="2" style="text-align:center;color:red;" >
               <a-icon type="delete" class="class-height-100"/>
             </a-col>
           </a-row>
@@ -30,7 +32,7 @@
     </a-row>
 
     <a-row>
-      <a-col :span="6" :offset="9">
+      <a-col :span="8" :offset="8">
         <a-row style="background-color: #f0f2f5;">
           <a-col class="left label" :span="12">Total:</a-col>
           <a-col class="right price label" style="text-align:right" :span="12">$90</a-col>
@@ -46,9 +48,9 @@
       <div class="card" v-for="item in todaySpecial">
         <a-row>
           <a-col :span="12">
-            Brand
+            <img src="../assets/coles_save.png">
           </a-col>
-          <a-col :span="12">
+          <a-col :span="12" style="text-align:right">
             <a-icon type="heart" />
           </a-col>
           <a-row>
@@ -57,7 +59,7 @@
             </a-col>
           </a-row>
           <a-row>
-            <a-col :span="24">
+            <a-col :span="24" class="text-wrap">
               {{item.name}}
             </a-col>
           </a-row>
@@ -136,13 +138,18 @@ h2 {
 .card {
   border: 2px solid #f0f2f5;
   width: 150px;
-  height: 175px;
+  height: 250px;
   margin: 0 5px;
-  padding: 2px;
+  padding: 5px;
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
 }
 
 .scrolling-wrapper, .scrolling-wrapper-flexbox {
-  height: 200px;
+  height: 250px;
   margin-bottom: 20px;
   width: 100%;
   -webkit-overflow-scrolling: touch;
@@ -166,6 +173,10 @@ h2 {
   padding: 5px;
   margin-bottom: 10px;
 }
+.cart-item img {
+  max-width: 100%;
+  max-height: 100%;
+}
 .price{
   color: #37ac70;
   font-size: large;
@@ -173,6 +184,11 @@ h2 {
 .discount{
   font-size: smaller;
   font-weight: 100;
+}
+.text-wrap{
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 
 </style>
